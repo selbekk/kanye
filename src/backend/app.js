d8295aed41507126be253d0f1ed1f77c73dd6836
selbekk <kristofer.selbekk@bekk.no>
2015-06-05 14:37:19 +0200

    Add a basic server

---
+++ src/backend/app.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    exphbs = require('express-handlebars'),
+    Logger = require('logbekk');
+
+var app = express(),
+    log = new Logger();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' });
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 4000);
+
+app.use('/assets', express.static('./../frontend/dist'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.listen(app.get('port'), function() {
+	log.info('server launched @ localhost:' + app.get('port'));
+});
