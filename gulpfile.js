e38f2709f80e5fbe74ff674e63026c0407470f8a
selbekk <kristofer.selbekk@bekk.no>
2015-06-05 16:49:27 +0200

    Set up basic Gulp frontend build

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    less = {
+        prefix: require('less-plugin-autoprefix'),
+        compile: require('gulp-less'),
+        minify: require('less-plugin-clean-css')
+    },
+    util = {
+        g: require('gulp-util')
+    };
+
+
+gulp.task('style', function() {
+    util.g.log('Compiling styles...');
+    gulp.src('./src/frontend/less/main.less')
+        .pipe(less.compile({
+            plugins: [
+                new less.prefix({ browsers: ["last 2 versions"] }),
+                new less.minify({ advanced: true })
+            ]})
+        )
+        .pipe(gulp.dest('./src/frontend/dist'));
+})
+
+gulp.task('default', [ 'style' ]);
